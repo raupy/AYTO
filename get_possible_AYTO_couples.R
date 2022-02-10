ed410f22326eba1b623b95c7bc61de8280f714cc
raupy <lilli.schmidt@stu.eh-berlin.de>
2022-02-10 21:59:07 +0100

    time for version control

---
+++ get_possible_AYTO_couples.R
@@ -0,0 +1,163 @@
+library(tidyr)
+#install.packages("DescTools")
+library(DescTools)
+
+if(!exists("readAYTO", mode="function")) source("read_AYTO_data.R")
+
+### ------------------------------------------------------------------------------
+### ------------------------------------------------------------------------------
+
+
+
+
+
+# this function is the heart of the script
+# it joins the possible combinations from two matching nights @df_night_1 and @df_night_2
+# TODO: explain how it works in general
+join_nights<- function(df_night_1, df_night_2){
+  inner <- df_night_1 %>%
+    inner_join(df_night_2, by = "comb") %>%
+    select(comb)
+  
+  filtered_night_join <- df_night_1 %>%
+    full_join(df_night_2, by = "comb") %>%
+    filter(is.na(night.x) | is.na(night.y)) 
+  
+  unique_combs <- filtered_night_join %>%
+    inner_join(filtered_night_join, by = c("night.x" = "night.y")) %>% # we are not really matching night numbers but NAs
+    transmute(whole_comb = paste(comb.x, comb.y)) %>%
+    remove_doubles() 
+  
+  rbind(unique_combs, inner) %>% 
+    mutate(is_collision = sapply(comb, is.collision)) %>% 
+    filter(is_collision == F) %>% 
+    transmute(comb, possible = sapply(comb, is.possible)) %>% 
+    filter(possible == T) %>% 
+    transmute(night = df_night_2$night[1], comb) %>%
+    arrange(comb)
+}
+
+# removes double entries from possible combinations -> only keeps unique combs
+# example: "Aurelia+Diogo Aurelia+Diogo Jacky+Salvo Jill+Manu Kathleen+Salvo" -> "Aurelia+Diogo Jacky+Salvo Jill+Manu Kathleen+Salvo"
+# it ignores that Salvo has two matches in this combination (Jacky+Salvo and Kathleen+Salvo) (the function "is.collision" deals with that)
+remove_doubles <- function(comb){
+  comb %>% 
+    separate(1, letters[1:11], sep = " ", extra = "drop", fill = "right") %>%
+    apply(1, function(x) paste(sort(na.omit(unique(x))), collapse = " ")) %>%
+    data.frame() %>%
+    rename(comb = 1)
+  #l <- list()
+  #for(i in 1:nrow(comb)){
+  #  vec <- sort(unique(unlist(str_split(comb[i,1], " "))))
+  #  l[[i]] <- paste(vec, collapse = " ")
+  #}
+  #unlist(l)
+}
+
+
+
+
+is.collision <- function(vec_of_couples){
+  vec_of_couples <- vec_of_couples[[1]] # it's a list with one element: the vector of couples
+  vec_of_couples <- unlist(str_split(vec_of_couples, fixed(" ")))
+  collision <- FALSE
+  special_person_detect <- sapply(vec_of_couples, str_detect, pattern = special_person)
+  if(sum(special_person_detect) == 1){ 
+    # the person she is with could have a second match, so don't set collision to true
+    # instead: remove her couple from the vector 
+    vec_of_couples <- vec_of_couples[-which(special_person_detect)]
+  }
+  solo_names <- unlist(str_split(vec_of_couples, fixed("+")))
+  if(length(solo_names) > length(unique(solo_names))){
+    collision <- TRUE
+  }
+  collision
+}
+
+
+is.possible <- function(comb){
+  is_possible <- TRUE
+  comb <- unlist(str_split(comb[[1]], fixed(" "))) #it is a list with one element: the vector of couples
+  for(i in 1:length(nights)){
+    if(!check.upper.bound(nights[[i]], comb, max_cap[i])){
+      is_possible <- FALSE
+      break
+    }
+  }
+  is_possible
+}
+
+
+check.upper.bound <- function(night, comb, upper_bound){
+  good_check <- FALSE
+  if(length(intersect(night, comb)) <= upper_bound){
+    good_check <- TRUE
+  } 
+  good_check
+}
+
+
+### ------------------------------------------------------------------------------
+### ------------------------------------------------------------------------------
+
+
+
+
+
+
+# transforms a CombSet matrix @sub (from the DescTools package) into a data frame
+# then pastes together all columns into one
+# the resulting df contains two columns: the @night_number as night and comb string from @sub
+# example:
+#   night                                      comb
+# 1     1       Aurelia+Diogo Jacky+Salvo Jill+Jamy
+# 2     1   Aurelia+Diogo Jacky+Salvo Kathleen+Manu
+# 3     1   Aurelia+Diogo Jacky+Salvo Steffi+Danilo
+df_from_sub <- function(sub, night_number){
+  data.frame(sub) %>%
+    unite(col = "comb", sep = " ") %>%
+    transmute(night = night_number, comb)
+}
+
+
+save_number_of_combinations <- function(df, special_person, perfect_matches){
+  df %>% 
+    mutate(n_whitespace = str_count(comb, " "),
+           contains_special_person = str_detect(comb, fixed(special_person))) %>% 
+    group_by(n_whitespace, contains_special_person) %>%
+    count() %>% 
+    mutate(n_couples = n_whitespace + as.numeric(!contains_special_person) + nrow(perfect_matches),
+           n_missing_couples = 10 - n_couples,
+           n_comb = n * factorial(n_missing_couples) * 10^(as.numeric(!contains_special_person))) %>%
+    ungroup() %>%
+    summarise(n_comb = sum(n_comb)) %>%
+    transmute(night = df$night[1], n_comb)
+}
+
+
+
+combinations <- function(nights, max_cap, special_person, perfect_matches){
+  start_sub <- CombSet(sort(nights[[1]]), m=max_cap[1]) 
+  dfx <- df_from_sub(start_sub,1)
+  number_of_combs <- tibble()
+  for(i in 2:length(nights)){
+    night <- nights[[i]]
+    sub <- CombSet(sort(night), m=max_cap[i])
+    dfy <- df_from_sub(sub,i)
+    dfx <- join_nights(dfx,dfy)
+    if(nrow(dfx) == 0){
+      #TODO ???
+    }
+    number_of_combs <- rbind(number_of_combs, save_number_of_combinations(dfx, special_person, perfect_matches))
+  }
+  list(dfx, number_of_combs)
+}
+
+
+### ------------------------------------------------------------------------------
+### ------------------------------------------------------------------------------
+
+nights <- all_nights_couples
+special_person = girls[11] # 11th candidate comes later to the cast -> one boy gets a second perfect match
+max_cap <- as.numeric(night_lights)
+combs2 <- combinations(nights,max_cap, special_person, perfect_matches)
---
+++ read_AYTO_data.R
@@ -0,0 +1,54 @@
+library(dplyr)
+library(readxl)
+library(tibble)
+library(stringr)
+library(assertive)
+
+### ------------------------------------------------------------------------------
+### functions for reading AYTO excel sheet and extracting data 
+
+readAYTO <- function(ayto_excel_file){
+  read_excel(ayto_excel_file) %>%
+    column_to_rownames(var = "boys") # get the boys column as row names
+}
+
+# extracts from the @ayto_data frame the couple of the @girl in night @x 
+get_couple_for_girl_in_night_x <- function(girl, x, ayto_data){
+  assert_all_are_positive(x)
+  girl_filtered <- ayto_data %>%
+    filter(str_detect(.data[[girl]], as.character(x)))
+  if(nrow(girl_filtered) == 0){ # a girl might not have a couple because of 11th female candidate
+    return("")
+  } 
+  paste(girl, rownames(girl_filtered)[1], sep = "+") # e.g. "Aurelia+Diogo"
+}
+
+# returns a chr with all the couples from matching night @night_nr using the @ayto_tbl and the vector of the @girls names
+filter_night_x <- function(night_nr, ayto_tbl, girls){
+  couples <- c()
+  couples <- sapply(girls, get_couple_for_girl_in_night_x, x = night_nr, ayto_data = ayto_tbl)
+  couples <- as.vector(couples)
+  couples[which(couples != "")] # remove empty string couples (girls without couple)
+}
+
+### ------------------------------------------------------------------------------
+### ------------------------------------------------------------------------------
+
+
+ayto_excel_file <- "ayto3.xlsx"
+ayto_tbl <- readAYTO(ayto_excel_file)
+
+
+night_lights <- read_excel(ayto_excel_file, sheet = "night_lights") 
+# this sheet contains a table with one row: the matching nights' numbers and their corresponding number of lights
+
+
+nights_in_the_future <- which(is.na(night_lights[1,])) # any missing data? (e.g. beginning of a season)
+night_lights <- night_lights[1, -nights_in_the_future] # ignore missing data = drop future nights
+
+girls = colnames(ayto_tbl)
+all_nights_couples <- sapply(1:ncol(night_lights), filter_night_x, ayto_tbl = ayto_tbl, girls = girls)
+# a list with all matching night couples in every matching night
+
+perfect_matches <- read_excel(ayto_excel_file, sheet = "perfect_matches") 
+# this sheet contains a table with one row: the matching nights' numbers and their corresponding number of lights
