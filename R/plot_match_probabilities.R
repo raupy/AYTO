c04adfa59fb158406f8ddd3d8e45b2a9f255936d
raupy <lilli.schmidt@stu.eh-berlin.de>
2022-05-10 13:40:39 +0200

    refactor: Improve plot function

--- R/plot_match_probabilities.R
+++ R/plot_match_probabilities.R
@@ -1,17 +1,17 @@
 library(ggplot2)
 library(ggthemes)
 
-
-plot_match_probabilities <- function(name_of_girl, sum_table) {
+plot_match_proportions <- function(candidate_name, sex, sum_table) {
   match_probs_plot <- sum_table %>% 
-    filter(girl == name_of_girl) %>%
+    filter(.data[[sex]] == candidate_name) %>%
     ggplot(aes(night, prob)) + 
     geom_point() + geom_line() +
+    labs(x = "Matching night", y = "Proportion of combinations") +
     scale_x_continuous(breaks = seq(0, 10, by = 2)) + 
-    scale_y_continuous(breaks = seq(0, 1, by = 0.1)) + 
-    ggtitle(name_of_girl) +
+    scale_y_continuous(breaks = seq(0, 1, by = 0.2)) + 
+    ggtitle(candidate_name) +
     facet_wrap(~boy, ncol = 5) +
     theme_stata()
-  ggsave(paste0("graphics/", tolower(name_of_girl), ".png"))
+  ggsave(paste0("graphics/", tolower(candidate_name), ".png"))
   match_probs_plot
 }
