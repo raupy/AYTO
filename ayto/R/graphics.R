208151f496ea0a482266152d8aa97415827d9e27
raupy <lilli.schmidt@stu.eh-berlin.de>
2022-07-05 15:15:58 +0200

    feat: Add Shinyapp Code

---
+++ ayto/R/graphics.R
@@ -0,0 +1,39 @@
+library(ggthemes)
+library(viridis)
+
+add_labels_to_plot <- function(plt, candidate_name) {
+  plt + 
+    labs(x = "Matching Night", y = "Anteil an möglichen Kombinationen") +
+    scale_x_continuous(breaks = seq(0, 10, by = 2)) + 
+    scale_y_continuous(breaks = seq(0, 1, by = 0.2)) + 
+    ggtitle(paste0(candidate_name, "s Perfect-Match-Entwicklung"))
+}
+
+filter_for_candidate <- function(candidate_name, sex, sum_table) {
+  sum_table %>% 
+    filter(.data[[sex]] == candidate_name)
+}
+
+
+plot_match_proportions_grid <- function(candidate_name, sex, sum_table) {
+  match_probs_plot <- filter_for_candidate(candidate_name, sex, sum_table) %>%
+    ggplot(aes(night, prob)) + 
+    geom_point() + geom_line() + 
+    facet_wrap(~ boy, ncol = 5) 
+  
+  match_probs_plot <- add_labels_to_plot(match_probs_plot, candidate_name) 
+  match_probs_plot
+}
+
+plot_match_proportions_area <- function(candidate_name, sex, sum_table) {
+  match_probs_plot <- filter_for_candidate(candidate_name, sex, sum_table) %>%
+    rename(Boy = boy) %>%
+    ggplot(aes(night, prob, fill = Boy)) + 
+    geom_area(alpha = 0.6 , size = 0.5, colour = "white") +
+    scale_fill_viridis(discrete = T) 
+  
+  match_probs_plot <- add_labels_to_plot(match_probs_plot, candidate_name)
+  match_probs_plot
+}
+
+#sapply(girls, plot_match_proportions_grid, "girl", summarized_table)
---
+++ ayto/R/read-AYTO-data.R
@@ -0,0 +1,63 @@
+library(readxl)
+library(tidyverse)
+library(assertive)
+### functions for reading AYTO excel sheet and extracting data 
+
+
+readAYTO <- function(ayto_excel_file) {
+  read_excel(ayto_excel_file) %>%
+    column_to_rownames(var = "boys") # get the boys column as row names
+}
+
+# extracts from the @ayto_data frame the couple of the @girl in night @x 
+get_couple_for_girl_in_night_x <- function(girl, x, ayto_data) {
+  assert_all_are_positive(x)
+  girl_filtered <- ayto_data %>%
+    filter(str_detect(.data[[girl]], as.character(x)))
+  if (nrow(girl_filtered) == 0) { # a girl might not have a couple because of 11th female candidate
+    return("")
+  } 
+  paste(girl, rownames(girl_filtered)[1], sep = "+") # e.g. "Aurelia+Diogo"
+}
+
+# returns a chr with all the couples from matching night @night_nr using the @ayto_tbl and the vector of the @girls names
+filter_night_x <- function(night_nr, ayto_tbl, girls) {
+  couples <- c()
+  couples <- sapply(girls, get_couple_for_girl_in_night_x, x = night_nr, ayto_data = ayto_tbl)
+  couples <- as.vector(couples)
+  couples[which(couples != "")] # remove empty string couples (girls without couple)
+}
+
+
+ayto_excel_file_names <- paste0("data/", dir("data"))
+ayto_excel_file <- ayto_excel_file_names[1]
+ayto_tbl <- readAYTO(ayto_excel_file)
+
+
+night_lights <- read_excel(ayto_excel_file, sheet = "night_lights") 
+# this sheet contains a table with one row: the matching nights' numbers and their corresponding number of lights
+
+nights_in_the_future <- which(is.na(night_lights[1, ])) # any missing data? (e.g. beginning of a season)
+night_lights <- night_lights[1, -nights_in_the_future] # ignore missing data = drop future nights
+max_cap <- as.numeric(night_lights)
+
+boys <- rownames(ayto_tbl)
+girls <- colnames(ayto_tbl)
+special_person <- girls[11] # 11th candidate comes later to the cast -> one boy gets a second perfect match
+nights <- lapply(1:ncol(night_lights), # a list with all matching night couples in every matching night
+                 filter_night_x, 
+                 ayto_tbl = ayto_tbl, girls = girls) 
+
+perfect_matches <- read_excel(ayto_excel_file, sheet = "perfect_matches") 
+no_matches <- read_excel(ayto_excel_file, sheet = "no_matches") 
+
+ayto_tbl <- ayto_tbl %>%
+  mutate(across(everything(),
+                ~ str_remove(.x, fixed("0"))
+                )
+  )
+#### read rds files ----
+
+combs <- readRDS("data/combs.rds")
+combs_dfs_list <- readRDS("data/combs_dfs_list.rds")
+summarized_table <- readRDS("data/sumtable.rds")
